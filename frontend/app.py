e4893dcb0935f3adfa71b6b21e4ba0472e54b962
burakarslan8 <burakarslan271@gmail.com>
2024-05-25 16:10:55 +0300

    pre-uploaded images feature

--- frontend/app.py
+++ frontend/app.py
@@ -5,23 +5,23 @@ import os
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads'
+PRE_UPLOADED_FOLDER = 'static/pre_uploaded_images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['PRE_UPLOADED_FOLDER'] = PRE_UPLOADED_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files or 'title' not in request.form:
-            return 'No file or title part'
-        file = request.files['file']
         title = request.form['title']
-        if file.filename == '':
-            return 'No selected file'
-        if file and title:
+        file = request.files.get('file')
+        pre_uploaded = request.form.get('pre_uploaded')
+        
+        if file and file.filename != '':
             try:
                 # Ensure the upload folder exists
                 if not os.path.exists(app.config['UPLOAD_FOLDER']):
                     os.makedirs(app.config['UPLOAD_FOLDER'])
-                
+
                 # Save the file to the static folder
                 filename = file.filename
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -30,10 +30,10 @@ def upload_file():
                 # Now that the file is saved, open it for the POST request
                 with open(file_path, 'rb') as img_file:
                     response = requests.post('http://127.0.0.1:5000/predict', files={'file': img_file}, data={'description': title})
-                
+
                 response.raise_for_status()  # Raise an error for bad status codes
                 data = response.json()
-                
+
                 # Pass the URL of the uploaded image
                 uploaded_image_url = url_for('static', filename='uploads/' + filename)
                 return render_template('index.html', best_food=data['predictions'][0], similarity_score=data['similarity_score'], uploaded_image_url=uploaded_image_url, title=title)
@@ -41,7 +41,26 @@ def upload_file():
                 return f'An error occurred: {e}'
             except ValueError:
                 return f'Error decoding JSON response: {response.content}'
-    return render_template('index.html')
+        elif pre_uploaded:
+            try:
+                pre_uploaded_image_path = os.path.join(app.config['PRE_UPLOADED_FOLDER'], pre_uploaded)
+                with open(pre_uploaded_image_path, 'rb') as img_file:
+                    response = requests.post('http://127.0.0.1:5000/predict', files={'file': img_file}, data={'description': title})
+
+                response.raise_for_status()  # Raise an error for bad status codes
+                data = response.json()
+
+                # Pass the URL of the pre-uploaded image
+                uploaded_image_url = url_for('static', filename='pre_uploaded_images/' + pre_uploaded)
+                return render_template('index.html', best_food=data['predictions'][0], similarity_score=data['similarity_score'], uploaded_image_url=uploaded_image_url, title=title)
+            except requests.exceptions.RequestException as e:
+                return f'An error occurred: {e}'
+            except ValueError:
+                return f'Error decoding JSON response: {response.content}'
+    
+    # List pre-uploaded images
+    pre_uploaded_images = os.listdir(app.config['PRE_UPLOADED_FOLDER'])
+    return render_template('index.html', pre_uploaded_images=pre_uploaded_images)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
